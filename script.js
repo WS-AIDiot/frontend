28650bfb7d70e0f5e4d1d1bdb3f96b03bb2e8be6
Anton <wrstrass@gmail.com>
2024-11-06 22:37:48 +0300

    Add fancy animations to the main page

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", (ev) => {
+    document.getElementById("robot").style.opacity = "1";
+    document.getElementById("text").style.transform = "translate(-50%)";
+    document.getElementById("text").style.opacity = "1";
+});
