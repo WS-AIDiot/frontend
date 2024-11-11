56dddaeff2c74d82943cd0eff525c4a61712a608
Anton <wrstrass@gmail.com>
2024-11-11 21:50:06 +0300

    Add window.local_storage facade

--- editor/script.js
+++ editor/script.js
@@ -1,3 +1,17 @@
+window.local_storage = {
+    get: (key, default_value) => {
+        let result = localStorage.getItem(key);
+        result = (result === null) ? default_value : JSON.parse(result);
+        return result;
+    },
+    set: (key, value) => {
+        let str = JSON.stringify(value);
+        localStorage.setItem(key, str);
+        return str;
+    }
+};
+
+
 window.addEventListener("load", () => {
     let active_editor = 0;
     let tabs = document.getElementsByClassName("tab");
