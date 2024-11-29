2c2d269d7e3ad1e580aa006b16ae33174364ab69
Anton <wrstrass@gmail.com>
2024-11-29 14:45:35 +0300

    Restructuring

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", (ev) => {
-    document.getElementById("robot").style.opacity = "1";
-    document.getElementById("text").style.transform = "translate(-50%)";
-    document.getElementById("text").style.opacity = "1";
-});
