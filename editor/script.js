16a788e20642beb9f22818d0a68b38d5b9f16e8b
Anton <wrstrass@gmail.com>
2024-12-03 20:45:14 +0300

    Editor: remove old list_documents

--- editor/script.js
+++ editor/script.js
@@ -224,30 +224,6 @@ window.addEventListener("load", async () => {
         });
     }
 
-    let documents_element = document.getElementById("documents");
-    (() => {
-        if (window.ENV.ENV === "static") {
-            return mock_fetch(window.local_storage.get("documents", []));
-        } else {
-            // alert("TODO");
-        }
-    })().then(response => {
-        return response.json();
-    }).then((json) => {
-        for (const document of json) {
-            console.log(document);
-            documents_element.innerHTML += `
-            <div class="document">
-                <img src="doc_icon.png" alt="Document Icon">
-                <div class="captions">
-                    <h2 class="title">${document.filename}</h2>
-                    <p class="date">${document.uploaded_at}</p>
-                </div>
-                <button class="select">Select</button>
-            </div>`;
-        }
-    });
-
     let upload = document.getElementById("upload");
     upload.addEventListener("change", (ev) => {
         let filename = upload.value.split("\\")[2];
